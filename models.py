32dc8f33022f752e2efe4b431ca9c31c39f8c05f
Knut Perseke <knut.perseke@gmail.com>
2017-08-22 11:21:03 +0200

    add new routes for viz

--- models.py
+++ models.py
@@ -1,8 +1,9 @@
 import itertools
 from plenartracker import db
+from datetime import datetime
 
-from sqlalchemy import ForeignKey, or_
-from sqlalchemy.orm import relationship, load_only, class_mapper
+from sqlalchemy import ForeignKey, or_, func
+from sqlalchemy.orm import relationship, load_only, Load, class_mapper, subqueryload
 
 
 class Speaker:
@@ -16,6 +17,40 @@ class Speaker:
                       Utterance.speaker_cleaned) \
             .all()
 
+class MdB(db.Model):
+    __tablename__ = "mdb"
+
+    id = db.Column(db.Integer, primary_key=True)
+    agw_id = db.Column(db.String)
+    profile_url = db.Column(db.String)
+    first_name = db.Column(db.String)
+    last_name = db.Column(db.String)
+    gender = db.Column(db.String)
+    birth_date = db.Column(db.Date)
+    education = db.Column(db.String)
+    picture = db.Column(db.String)
+    party = db.Column(db.String)
+    election_list = db.Column(db.String)
+    list_won = db.Column(db.String)
+    top_id = db.Column(db.Integer)
+
+    @staticmethod
+    def get_all():
+        return db.session.query(MdB) \
+            .all()
+
+    @staticmethod
+    def get_all_by(column):
+        query = db.session.query(getattr(MdB, column), func.count(getattr(MdB, column))) \
+            .group_by(getattr(MdB, column)) \
+            .all()
+        result = {}
+        for entry, count in query:
+            result[entry] = count
+        return result
+
+    def __repr__(self):
+        return '<MdB {}-{}-{}>'.format(self.first_name, self.last_name, self.party)
 
 class Utterance(db.Model):
     __tablename__ = "de_bundestag_plpr"
@@ -32,7 +67,7 @@ class Utterance(db.Model):
     text = db.Column(db.String)
     top_id = db.Column(db.Integer, ForeignKey("tops.id"))
     top = relationship("Top")
-    agw_url = None
+    speaker_key = db.Column(db.Integer)
 
     @staticmethod
     def get_all(wahlperiode, session):
@@ -41,6 +76,81 @@ class Utterance(db.Model):
             .filter(Utterance.wahlperiode == wahlperiode) \
             .order_by(Utterance.sequence) \
             .all()
+
+    @staticmethod
+    def all_by_gender_category_count():
+
+        subquery = db.session.query(Utterance.sitzung.label("sitzung"), Utterance.wahlperiode, Utterance.speaker_cleaned, MdB.gender, Top.category, Top.number) \
+            .filter(Utterance.speaker_key == MdB.id) \
+            .filter(Utterance.top_id == Top.id) \
+            .filter(Utterance.type == "speech") \
+            .group_by(MdB.gender, Top.category, Utterance.sitzung, Utterance.wahlperiode, Utterance.speaker_cleaned, Top.number) \
+            .subquery()
+
+        query = db.session.query(subquery.c.category, subquery.c.gender, func.count(subquery.c.category)) \
+            .group_by(subquery.c.gender, subquery.c.category) \
+            .all()
+
+        result = []
+        for category, gender, count in query:
+            result.append({"category": category, "gender": gender, "count": count})
+        return result
+
+    @staticmethod
+    def all_by_age_cetegory_count():
+        from_sq = db.session.query(Utterance.sitzung, Utterance.wahlperiode, Utterance.speaker_cleaned, MdB.birth_date, Top.category, Top.number) \
+            .filter(Utterance.speaker_key == MdB.id) \
+            .filter(Utterance.top_id == Top.id) \
+            .filter(Utterance.type == "speech") \
+            .group_by(MdB.birth_date, Top.category, Utterance.sitzung, Utterance.wahlperiode, Utterance.speaker_cleaned, Top.number) \
+            .subquery()
+
+        query = db.session.query(from_sq.c.category, from_sq.c.birth_date) \
+            .all()
+
+        result = []
+        for category, date in query:
+            result.append({"category": category, "date": date.year})
+        return result
+
+    @staticmethod
+    def all_by_education_category_count():
+
+        subquery = db.session.query(Utterance.sitzung, Utterance.wahlperiode, Utterance.speaker_cleaned, MdB.education, Top.category, Top.number) \
+            .filter(Utterance.speaker_key == MdB.id) \
+            .filter(Utterance.top_id == Top.id) \
+            .filter(Utterance.type == "speech") \
+            .group_by(MdB.education, Top.category, Utterance.sitzung, Utterance.wahlperiode, Utterance.speaker_cleaned, Top.number) \
+            .subquery()
+
+        query = db.session.query(subquery.c.category, subquery.c.education, func.count(subquery.c.category)) \
+            .group_by(subquery.c.education, subquery.c.category) \
+            .all()
+
+        result = []
+        for category, education, count in query:
+            result.append({"category": category, "education": education, "count": count})
+        return result
+
+    @staticmethod
+    def all_by_election_list_category_count():
+
+        subquery = db.session.query(Utterance.sitzung, Utterance.wahlperiode, Utterance.speaker_cleaned, MdB.election_list, Top.category, Top.number) \
+            .filter(Utterance.speaker_key == MdB.id) \
+            .filter(Utterance.top_id == Top.id) \
+            .filter(Utterance.type == "speech") \
+            .group_by(MdB.election_list, Top.category, Utterance.sitzung, Utterance.wahlperiode, Utterance.speaker_cleaned, Top.number) \
+            .subquery()
+
+        query = db.session.query(subquery.c.category, subquery.c.election_list, func.count(subquery.c.category)) \
+            .group_by(subquery.c.election_list, subquery.c.category) \
+            .all()
+
+        result = []
+        for category, election_list, count in query:
+            result.append({"category": category, "election_list": election_list, "count": count})
+        return result
+
 
     def to_json(self):
         d = {}
@@ -69,6 +179,8 @@ class Top(db.Model):
     detail = db.Column(db.String)
     year = db.Column(db.Integer)
     category = db.Column(db.String)
+    duration = db.Column(db.String)
+    held_on = db.Column(db.Date)
 
     @staticmethod
     def get_all(search=None, people=None, years=None, categories=None):
@@ -102,6 +214,10 @@ class Top(db.Model):
         return sorted(results, key=lambda entry: (entry["session"]["wahlperiode"], entry["session"]["sitzung"]))
 
     @staticmethod
+    def get_all_plain():
+        return db.session.query(Top).all()
+
+    @staticmethod
     def get_categories():
         db_topics = db.session.query(Top) \
                 .distinct(Top.category) \
@@ -111,3 +227,48 @@ class Top(db.Model):
             topics.update(row.category.split(";"))
         topics.discard("")
         return list(topics)
+
+    @staticmethod
+    def sum_by_category(sum_by=None):
+        data = Top.split_by_category()
+        results = {}
+        for row in data:
+            cat = row['category']
+            if cat not in results.keys():
+                results[cat] = 0
+            results[cat] += row['duration']
+        return results
+
+    @staticmethod
+    def count_by_category(sum_by=None):
+        data = Top.split_by_category()
+        results = {}
+        for row in data:
+            cat = row['category']
+            if cat not in results.keys():
+                results[cat] = 0
+            results[cat] += 1
+        return results
+
+    @staticmethod
+    def split_by_category():
+        db_topics = db.session.query(Top).filter(Top.category != '').all()
+
+        result = []
+
+        for row in db_topics:
+            categories = row.category.split(";")
+            for category in categories:
+                curr = Top.row2dict(row)
+                curr['category'] = category
+                result.append(curr)
+        return result
+
+    @staticmethod
+    def row2dict(row):
+        d = {}
+        for column in row.__table__.columns:
+            d[column.name] = getattr(row, column.name)
+
+        return d
+
--- views.py
+++ views.py
@@ -4,7 +4,8 @@ import os
 from flask import render_template, request, jsonify
 
 from plenartracker import app
-from models import Utterance, Top, Speaker
+from models import Utterance, Top, Speaker, MdB
+from datetime import datetime
 
 
 def get_mdbs():
@@ -37,48 +38,6 @@ def index():
 def protokolle():
     return render_template("protokolle.html", title="Protokolle", selected="protokolle")
 
-@app.route("/analyse")
-def analyse():
-    viz = [
-        {
-            "description": "Welche Themen wurden wann besprochen?",
-            "categories": ["Featured", "Themen"],
-            "image": "/static/img/timeline_icon.png",
-            "link": "timeline"
-        },
-        {
-            "description": "Wer sprach am häufigsten zum Thema deiner Wahl?",
-            "categories": ["Featured", "Themen"],
-            "image": "/static/img/victory_icon.png",
-            "link": "person-amount-subject"
-        },
-        {
-            "description": "Wieviel Zeit gab es für das Thema deiner Wahl?",
-            "categories": ["Featured", "Themen"],
-            "image": "/static/img/fun_ico",
-            "link": "time-subject"
-        },
-        {
-            "description": "Welches Thema wurde am häufigsten besprochen?",
-            "categories": ["Featured", "Themen"],
-            "image": "/static/img/fun_icon.png",
-            "link": "amount-subject"
-        },
-        {
-            "description": "Wie alt sind die Sprecher zu welchen Themen?",
-            "categories": ["Featured", "Themen"],
-            "image": "/static/img/group_icon.png",
-            "link": "speaker-age"
-        },
-        {
-            "description": "Wer stimmt entgegen der Parteilinie?",
-            "categories": ["Featured", "Themen"],
-            "image": "/static/img/fun_icon.png",
-            "link": "vote"
-        }
-    ]
-    return render_template("analyse.html", title="Analyse", selected="analyse", viz=viz)
-
 @app.route("/tracker")
 def tracker():
     return render_template("tracker.html", title="Tracker", selected="tracker")
@@ -96,8 +55,8 @@ def viz_test():
     return render_template("analyse/redeanteile.html", title="Analyse")
 
 
-@app.route("/api/tops")
-def api_tops():
+@app.route("/api/tops/")
+def api_tops_grouped():
     search = request.args.get("search")
     people = request.args.getlist("people")
     years = request.args.getlist("years")
@@ -129,3 +88,92 @@ def api_utterances(session_id):
 def api_categories():
     categories = Top.get_categories()
     return jsonify(data=categories)
+
+@app.route("/api/tops/category_sum")
+def api_tops_sum_by_category():
+    return jsonify(Top.sum_by_category())
+
+@app.route("/api/tops/category_count")
+def api_tops_count_by_category():
+    return jsonify(Top.count_by_category())
+
+@app.route("/api/utterances/by_gender_category")
+def api_uterrances():
+    result = {}
+    data = Utterance.all_by_gender_category_count()
+    for item in data:
+        categories = item['category'].split(";")
+        for category in categories:
+            if category not in result.keys():
+                result[category] = { 'male': 0, 'female':0 }
+
+            result[category][item['gender']] += item['count']
+    return json.dumps(result)
+
+@app.route("/api/utterances/by_birth_date_category")
+def api_utterances_birth_date_category():
+    data = Utterance.all_by_age_cetegory_count()
+    result = {}
+    for item in data:
+        categories = item['category'].split(";")
+        for category in categories:
+            if category not in result.keys():
+                result[category] = { }
+
+            group = int((datetime.now().year - item['date']) / 10) * 10
+            grouplabel = '{} - {}'.format(group, group + 9)
+            if grouplabel not in result[category]:
+                result[category][grouplabel] = 0
+            result[category][grouplabel] += 1
+    return json.dumps(result)
+
+@app.route("/api/utterances/by_profession_category")
+def api_utterances_profession_category():
+    result = {}
+    data = Utterance.all_by_education_category_count()
+    for item in data:
+        categories = item['category'].split(";")
+        for category in categories:
+            if category not in result.keys():
+                result[category] = result[category] = {}
+            education = item['education']
+            if education not in result[category]:
+                result[category][education] = 0
+            result[category][education] += item['count']
+    return json.dumps(result)
+
+@app.route("/api/utterances/by_election_list_category")
+def api_utterances_election_list_category():
+    result = {}
+    data = Utterance.all_by_election_list_category_count()
+    for item in data:
+        categories = item['category'].split(";")
+        for category in categories:
+            if category not in result.keys():
+                result[category] = result[category] = {}
+            election_list = item['election_list']
+            if election_list not in result[category]:
+                result[category][election_list] = 0
+            result[category][election_list] += item['count']
+    return json.dumps(result)
+
+@app.route("/api/mdb/aggregated")
+def api_mdb_aggregated():
+    column = request.args.get("attribute")
+    return json.dumps(MdB.get_all_by(column))
+
+@app.route("/api/mdb/aggregated/age")
+def api_mdb_aggregated_age():
+    data = MdB.get_all_by("birth_date")
+    result = {}
+    for date, count in data.items():
+        group = int((datetime.now().year - date.year) / 10) * 10
+        grouplabel = '{} - {}'.format(group, group + 9)
+        if grouplabel not in result:
+            result[grouplabel] = 0
+        result[grouplabel] += count
+    return json.dumps(result)
+
+@app.route("api/mdb/speech")
+def api_mdb_speech():
+    data = MdB.get_by_speech_frequency()
